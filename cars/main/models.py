cb50314f6def66f553e4967ebe3b38159490819d
sujit <sujitramdhakal59@gmail.com>
2023-08-13 15:29:10 +0545

    main page and search created

--- cars/main/models.py
+++ cars/main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Cars(models.Model):
+    carname = models.CharField(max_length=50, default='abc')
+    cardesc = models.CharField(max_length=1000, default='abc')
+    carimage = models.ImageField(upload_to='media/images')
+
+    def __str__(self) -> str:
+        return self.carname
--- cars/main/views.py
+++ cars/main/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from main.models import Cars
 # Create your views here.
 
 
 def main_page(request):
-    return render(request, 'main/index.html')
+    if request.method == 'POST':
+        # data = request.POST
+        car_name = request.POST.get('carname')
+        car_desc = request.POST.get('car_desc')
+        car_image = request.FILES.get('image')
+
+        Cars.objects.create(
+            carname=car_name, cardesc=car_desc, carimage=car_image)
+        return redirect('/main/')
+
+    cars = Cars.objects.all()
+    if request.GET.get('search_car'):
+        cars = Cars.objects.filter(
+            carname__icontains=request.GET.get('search_car'))
+    context = {'cars': cars}
+    return render(request, 'main/index.html', context)
