dacc0045340841ee9b2623d1e0354805821e72c9
sujit <sujitramdhakal59@gmail.com>
2023-08-18 10:20:10 +0545

    login and register added

--- cars/login/views.py
+++ cars/login/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib.auth import authenticate
 # Create your views here.
 
 
 def login_page(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            return redirect('/main/')
+        else:
+            return HttpResponse("username or password error")
+
     return render(request, 'login/index.html')
--- cars/register/views.py
+++ cars/register/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 
 def register_page(request):
+
+    if request.method == 'POST':
+        firstname = request.POST.get('firstname')
+        lastname = request.POST.get('lastname')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        cpassword = request.POST.get('cpassword')
+        if cpassword == password:
+            user = User.objects.create(
+                username=username, email=email, first_name=firstname, last_name=lastname)
+            user.set_password(password)
+            user.save()
+        else:
+            return HttpResponse("Passwords do not match")
+
+        return redirect('/login/')
+
     return render(request, 'register/index.html')
